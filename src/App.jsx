6c4f6bfc0025682107c69dd6b9efce86de094ad6
SAWADOGO Rimsomgdo Roger <rimssawadogo@gmail.com>
2024-08-09 09:54:29 +0000

    second

--- src/App.jsx
+++ src/App.jsx
@@ -32,7 +32,7 @@ function App() {
   }, []);
 
   useEffect(() => {
-    const ws = new WebSocket('ws://smartagriculture-backend.onrender.com/');
+    const ws = new WebSocket('wss://smartagriculture-backend.onrender.com/');
 
     ws.onopen = () => {
       console.log('WebSocket connected');
